66024b2be1b7dbfc3873526c1f81f74160399860
Mark Pollack <mpollack@pivotal.io>
2020-09-22 13:22:31 -0400

    remove deployment artifacts, change application.yml to use service DNS names

--- build-paketo.sh
+++
@@ -1,5 +0,0 @@
-# Building and publishing with cloud-native buildpacks
-
-# buildpack in use: cloudfoundry/cnb:bionic
-# published to https://hub.docker.com/repositories/triathlonguy 
-pack build triathlonguy/todos-api --publish --path . --builder gcr.io/paketo-buildpacks/builder:base --env BP_JAVA_VERSION=“8*”
--- build.sh
+++
@@ -1,4 +0,0 @@
-./mvnw clean install
-
-docker build -t triathlonguy/todos-api .
-docker push triathlonguy/todos-api:latest
